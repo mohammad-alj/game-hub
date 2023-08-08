879e6a29b6c82c4a389f37f2901722da7a6160e1
mohammad_alj <xqg1122@gmail.com>
2023-08-08 05:04:34 +0300

    Remove unused imports.

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,4 +1,4 @@
-import {Box, Button, Menu, MenuButton, MenuItem, MenuList, Skeleton} from '@chakra-ui/react';
+import {Button, Menu, MenuButton, MenuItem, MenuList} from '@chakra-ui/react';
 import {BsChevronDown} from 'react-icons/bs';
 import usePlatforms, {Platform} from '../hooks/usePlatforms';
 
